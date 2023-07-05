3955558649257717597986ae51587915f300f3dc
mrzhang <zhangmingshuo@cqcsmc.com>
2023-07-05 09:19:47 +0800

    1.3.7 线程创建方法三：使用 Callable 和 FutureTask 创建线程

--- src/main/java/CreateDemoByCallableAndFuture.java
+++ src/main/java/CreateDemoByCallableAndFuture.java
@@ -1,3 +1,5 @@
+//Nien Notebook 2 Chapter 1
+//1.3.7 线程创建方法三：使用 Callable 和 FutureTask 创建线程
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
@@ -11,34 +13,32 @@ public class CreateDemoByCallableAndFuture {
         //②编写好异步执行的具体逻辑，并且可以有返回值
         public Long call() throws Exception{
             long startTime = System.currentTimeMillis();
-             System.out.println(Thread.currentThread().getName()+ " 线程运行开始.");
+            System.out.println(Thread.currentThread().getName()+ " 线程运行开始.");
             Thread.sleep(10000);
             for (int i = 0; i < COMPUTE_TIMES; i++) {
                 int j = i * 10000;
             }
             long used = System.currentTimeMillis() - startTime;
-             System.out.println(Thread.currentThread().getName()+ " 线程运行结束.");
+            System.out.println(Thread.currentThread().getName()+ " 线程运行结束.");
             return used;
         }
     }
-    public static void main(String args[]) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         ReturnableTask task=new ReturnableTask();//③
-        FutureTask<Long> futureTask = new FutureTask<Long>(task);//④
+        FutureTask<Long> futureTask = new FutureTask<>(task);//④
         Thread thread = new Thread(futureTask, "returnableThread");//⑤
         thread.start();//⑥
         Thread.sleep(2500);
-         System.out.println(Thread.currentThread().getName()+ " 让子弹飞一会儿.");
-         System.out.println(Thread.currentThread().getName()+ " 做一点自己的事情.");
+        System.out.println(Thread.currentThread().getName()+ " 让子弹飞一会儿.");
+        System.out.println(Thread.currentThread().getName()+ " 做一点自己的事情.");
         for (int i = 0; i < COMPUTE_TIMES / 2; i++) {
             int j = i * 10000;
         }
-         System.out.println(Thread.currentThread().getName()+ " 获取并发任务的执行结果.");
+        System.out.println(Thread.currentThread().getName()+ " 获取并发任务的执行结果.");
 
         try {
             System.out.println(thread.getName()+"线程占用时间："+ futureTask.get());
-         } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
         System.out.println(Thread.currentThread().getName()+ " 运行结束.");
