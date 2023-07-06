c7647890ef24732fb7225ac84adf4543fcc50a79
ZhangMingShuo <zhangms97@163.com>
2023-07-06 10:28:43 +0800

    1.8.1 ThreadLocal 的基本使用

---
+++ src/main/java/ThreadLocalDemo.java
@@ -0,0 +1,64 @@
+import lombok.Data;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadLocalDemo {
+    @Data
+    static class Foo
+    {
+        //实例总数
+        static final AtomicInteger AMOUNT = new AtomicInteger(0);
+        //对象的编号
+        int index = 0;
+        //对象的内容
+        int bar = 10;
+        //构造器
+        public Foo()
+        {
+            index = AMOUNT.incrementAndGet(); //总数增加，并且给对象的编号
+        }
+        @Override
+        public String toString()
+        {
+            return index + "@Foo{bar=" + bar + '}';
+        }
+    }
+    //定义线程本地变量
+    private static final ThreadLocal<Foo> LOCAL_FOO = new
+            ThreadLocal<Foo>();
+    public static void main(String[] args) throws InterruptedException
+    {
+        //获取自定义的混合型线程池
+        ThreadPoolExecutor threadPool =
+                ThreadUtil.getMixedTargetThreadPool();
+        //提交 5 个任务，将会用到 5 个线程
+        for (int i = 0; i < 5; i++)
+        {
+            threadPool.execute(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    //获取“线程本地变量”中当前线程所绑定的值
+                    if (LOCAL_FOO.get() == null)
+                    {
+                        //设置“线程本地变量”中当前线程所绑定的值
+                        LOCAL_FOO.set(new Foo());
+                    }
+                    Print.tco("初始的本地值：" + LOCAL_FOO.get());
+                    //每个线程执行 10 次
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Foo foo = LOCAL_FOO.get();
+                        foo.setBar(foo.getBar() + 1); //值增 1
+                        ThreadUtil.sleepMilliSeconds(10);
+                    }
+                    Print.tco("累加 10 次之后的本地值：" + LOCAL_FOO.get());
+                    //删除“线程本地变量”中当前线程所绑定的值
+                    LOCAL_FOO.remove(); //这点对于线程池中的线程尤其重要
+                }
+            });
+        }
+    }
+}
--- src/main/java/ThreadUtil.java
+++ src/main/java/ThreadUtil.java
@@ -169,6 +169,59 @@ public class ThreadUtil {
     {
         return Thread.currentThread().getName();
     }
+    /**
+     * 空闲保活时限，单位秒
+     */
+    private static final int KEEP_ALIVE_SECONDS = 30;
 
+    /**
+     * 有界队列size
+     */
+    private static final int QUEUE_SIZE = 10000;
+    public static final String MIXED_THREAD_AMOUNT = "mixed.thread.amount";
+    private static final int MIXED_MAX = 128;  //最大线程数
+
+    //懒汉式单例创建线程池：用于混合型任务
+    private static class MixedTargetThreadPoolLazyHolder
+    {
+        //首先从环境变量 mixed.thread.amount 中获取预先配置的线程数
+        //如果没有对 mixed.thread.amount 做配置，则使用常量 MIXED_MAX 作为线程数
+        private static final int max = (null != System.getProperty(MIXED_THREAD_AMOUNT)) ?
+                Integer.parseInt(System.getProperty(MIXED_THREAD_AMOUNT)) : MIXED_MAX;
+        //线程池： 用于混合型任务
+        private static final ThreadPoolExecutor EXECUTOR = new ThreadPoolExecutor(
+                max,
+                max,
+                KEEP_ALIVE_SECONDS,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue(QUEUE_SIZE),
+                new CustomThreadFactory("mixed"));
+
+        static
+        {
+            EXECUTOR.allowCoreThreadTimeOut(true);
+            //JVM关闭时的钩子函数
+            Runtime.getRuntime().addShutdownHook(new ShutdownHookThread("混合型任务线程池", new Callable<Void>()
+            {
+                @Override
+                public Void call() throws Exception
+                {
+                    //优雅关闭线程池
+                    shutdownThreadPoolGracefully(EXECUTOR);
+                    return null;
+                }
+            }));
+        }
+    }
+
+    /**
+     * 获取执行混合型任务的线程池     *
+     *
+     * @return
+     */
+    public static ThreadPoolExecutor getMixedTargetThreadPool()
+    {
+        return MixedTargetThreadPoolLazyHolder.EXECUTOR;
+    }
 
 }
